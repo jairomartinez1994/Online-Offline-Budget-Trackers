150def83eb5b4651fbdc27e12fd763a035548343
Jairo Martinez <61437470+jairomartinez1994@users.noreply.github.com>
2020-08-05 14:03:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//  useNewUrlParser: true,
+//  useFindAndModify: false
+// });
 
 mongoose.connect(MONGODB_URI);
 
